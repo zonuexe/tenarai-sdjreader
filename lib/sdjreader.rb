addcb2170e20d9c05a8b54cfb831f93d8b3967bf
USAMI Kenta <tadsan@zonu.me>
2012-08-16 20:07:28 +0900

    commit

--- lib/sdjreader.rb
+++ lib/sdjreader.rb
@@ -4,6 +4,7 @@ require 'mechanize'
 require 'uri'
 require 'optparse'
 require 'singleton'
+require 'open-uri'
 
 module Sdjreader
   SITE_URI = 'http://slashdot.jp/'
@@ -12,12 +13,20 @@ module Sdjreader
     include Singleton
     def initialize
       @config = Hash.new
+      @index = IndexReader.instance
       @parser = OptionParser.new("/.J Reader - #{VERSION}")
       @parser.on("-h", "--help"){ @config[:help] = true }
       @parser.on("-v", "--version"){ @config[:version] = true }
     end
 
+    def main
+      if @config[:null]
+        @index.page
+      end
+    end
+
     def optparse(*args)
+      @config[:noargs] = true && return if args.size == 0
       @parser.parse! args
       @config[:rest] = args
     end
@@ -25,6 +34,7 @@ module Sdjreader
     def setup
       Dir.mkdir TEMP_DIR unless Dir.exists? TEMP_DIR
     end
+
     def pre(*args)
       config = self.optparse(*args)
       self.help if @config[:help]
@@ -37,14 +47,16 @@ module Sdjreader
     end
   end
 
-  class Reader
+  class IndexReader
+    include Singleton
     def initialize
       @agent = Mechanize.new
     end
     def get (*args)
       target = args[0] ? URI.join(SITE_URI, args[0]) : SITE_URI
-      @agent.get target
-      @agent.page
+      open(target) do |f|
+        Nokogiri::HTML f.read
+      end
     end
 
     def uri2filename(path)
@@ -56,11 +68,16 @@ module Sdjreader
     def get_haedline
       get.search(".story a").map{|g| {title: g.children[0].to_s , uri: g.attributes['href'].value}}
     end
+
+    def page
+      IndexReader.new.get_haedline.each_with_index{ |c, i|
+        puts "#{"%2d" % (i + 1)} #{c[:title]}"
+      }
+    end
   end
 end
 
 core = Sdjreader::Core.instance
 core.pre *ARGV
-Sdjreader::Reader.new.get_haedline.each_with_index{ |c, i|
-  puts "#{"%2d" % (i + 1)} #{c[:title]}"
-}
+core.main
+
